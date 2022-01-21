65b489800ce58fee74bfe7a79ef66195cd309313
moreirathais <thais_mjs@hotmail.com>
2022-01-20 21:15:37 -0300

    Primeira lógicas das minhas funções

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,17 @@
+const alfabeto =[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
+let codigoASC = (alfabeto.length-1)+65;
+let codigoLetra =alfabeto.length-1;
+let deslocamento = input.getElementByID("deslocamento");
+let frase = document.getElementById("cifrada");
+const cipher = (codigoASC-codigoLetra+deslocamento)%26 - codigoLetra;
 
-const cipher = {codigoASC-codigoLetra + deslocamento.giro-codigoLetra
-};
+function frase (i) {(frase[i]-65 + 2) %26+65;{
+    if (onclick){cipher;
+        return document.write("msg2");
+        
+    }
+   
+    }
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import cipher from './cipher.js';
-const alfabeto =[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
-let codigoASC = (alfabeto.length-1)+65;
-let codigoLetra =alfabeto.length-1;
-let deslocamento = input.getElementByID(deslocamento);
-let giro = 26;
+
+
 
 console.log(cipher);
