5b36c4b597d2e45abc991d6a81c74457691f7c36
moreirathais <thais_mjs@hotmail.com>
2022-01-30 13:51:49 -0300

    Código com JS funcionando

--- src/cipher.js
+++ src/cipher.js
@@ -1,85 +1,45 @@
 const cipher = {
-  encode(quantidade, texto){
-  let resultadoEncode = "";
-    for (let i=0; i<texto.length; i++){
-    let códigoAsc = texto.charCodeAt(i);   
-    let codigoLetra = 65;
-    let valorEncode = ((codigoASC - codigoLetra + quantidade)%26 + codigoLetra);
-    resultadoEncode = resultadoEncode.concat(texto.fromCharCode(valorEncode));
-    }
-    return resultadoEncode;
-    },
- decode(quantidade,textoCifrado){
-   let resultadoDecode = "";
-   for (let i=0; i< textoCifrado.length; i++){
-    let codigoASC =tecoCifrado.charCodeAt(i)
-       let codigoLetra = 90
-       let valorDecode= ((codigoASC - codigoLetra - quantidade)%26 + codigoLetra);
-       resultadoDecode = resultadoDecode.concat(textoCifrado.fromCharCode(valorDecode));
-    }
-
-    return resultadoDecode;
-
-    }
-}
-
-   export default cipher;
-
+     encode:
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//const alfabeto =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//const msg1 = document.getElementById("cifrar");
-//const msg2 = document.getElementById("cifrada");
-
-//msg1.addEventListener("submit", event => {
-//   event.//preventDefault();
- //   msg2 = [...(msg1).document.getElementById("cifrar").value].map(char => encrypt(char)).join("");
-//});
-
-//function encode (msg1){
-  //  const deslocamento = Number(form.input.value);
-    //if(alfabeto.includes);
-
-    //const posicao= alfabeto.indexOf;
-    //const novaPosicao = (posicao+deslocamento)%26;
-    //return alfabeto[novaPosicao]
-
-//}
-
-
-//let codigoASC = str.charCodeAt(alfabeto);
-//let codigoLetra =alfabeto.length-1;
-//let deslocamento = input.getElementByID("deslocamento");
-//let frase = document.getElementById("cifrada");
-//const cipher = (codigoASC-codigoLetra+deslocamento)%26 - codigoLetra;
-
-//function encode (i) {(frase[i]-65 + 2) %26+65;{
-   // if (onclick){cipher;
-   //     return document.write("msg2");
+      function (numDesloc, msgUsuario) {
         
-  //  }
-
-  //  function decode (i) {(frase[i]-65 - 2) %26+65;{
-   //     if (onclick){cipher;
-   //         return document.write("msg2");
-            
-    //    }   
-//
-
+        let msgCifrada = "";
+  
+        for (let i = 0; i < msgUsuario.length; i++) {
+          let cifrando = msgUsuario[i].charCodeAt();
+          if (cifrando >= 65 && cifrando <= 90) {
+            cifrando = ((cifrando - 65 + numDesloc) % 26) + 65
+          }
+          else if (cifrando >= 97 && cifrando <= 122) {
+            cifrando = ((cifrando - 97 + numDesloc) % 26) + 97
+          }
+        
+  
+          msgCifrada += String.fromCharCode(cifrando);
+        }
+        return msgCifrada;
+      },
+  
+    decode:
+  
+      function (numDesloc, msgUsuario) {
+        
+        let msgDecifrada = "";
+  
+        for (let i = 0; i < msgUsuario.length; i++) {
+          let decifrando = msgUsuario[i].charCodeAt();
+          if (decifrando >= 65 && decifrando <= 90) {
+            decifrando = ((decifrando + 65 - numDesloc) % 26) + 65
+          }
+  
+          else if (decifrando >= 97 && decifrando <= 122) {
+            decifrando = 122 - ((122 - decifrando + numDesloc) % 26)
+          }
+  
+          msgDecifrada += String.fromCharCode(decifrando);
+        }
+        return msgDecifrada;
+      }
+  };
+  
+   export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import cipher from './cipher.js';
 
-const elemento = document.getElementById ("button");
-elemento.addEventListener("click", enviar);
-function enviar(){
-    console.log("Entrou")
- 
-    let texto = document.getElementById("cifrar").value.toUpperCase();
-    document.getElementById("cifrada").value = texto
-}
-
-let quantidade = document.getElementById("descolamento");
-function cifrar(){
-    document.getElementById("cifrada").value = cipher.encode(quantidade, texto);
-
-    let textoCifrado = document.getElementById("cifrada").value.toUpperCase();
-}
-
-function cifrar ()
-
-
 console.log(cipher);
+
+const botao1 = document.getElementById("button1")
+const botao2 = document.getElementById("button2")
+const campoEntrada = document.getElementById("mensagem");
+const numDesloc = document.getElementById("deslocamento");
+const resultado = document.getElementById("Cifrada");
+
+
+
+botao1.addEventListener("click", function () {
+  resultado.value = cipher.encode(parseInt(numDesloc.value), campoEntrada.value);
+});
+
+botao2.addEventListener("click", function () {
+  resultado.value = cipher.decode(parseInt(numDesloc.value), resultado.value);
+});
+
+
+//const elemento = document.getElementById ("button");
+//elemento.addEventListener("click", enviar);
+
+//function enviar(){
+  //  let texto = document.getElementById("cifrar").value.toUpperCase();
+    //document.getElementById("cifrada").value = texto
+    //let quantidade = document.querySelector("#deslocamento").value
+   
+ //}
+
+//function cifrar(){
+  //  let texto = document.getElementById("cifrar").value.toUpperCase();
+    //document.getElementById("cifrada").value = texto
+  //  let quantidade = document.querySelector("#deslocamento").value
+    //texto = cipher.encode(quantidade, texto);
+    
+//}
+
+
+
+//function decifrar(){
+  //  let textoCifrado = document.getElemnteById("cifrada").value.toUpperCase();
+    //document.getElementById("decifrada").value = textoCifrado
+   // let quantidade = document.querySelector("#deslocamento").value
+    //textoCifrado= cipher.decode(quantidade, textoCifrado)
+
+//}
+
+
+
